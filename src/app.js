de7189b798dc3833ba69ef838425f0e26928bb47
juanantonio.capel@hotmail.com <juanantonio.capel@hotmail.com>
2020-05-05 11:36:28 +0100

    blah 3

--- src/app.js
+++ src/app.js
@@ -17,10 +17,17 @@ app.set('views', path.join(__dirname, 'views'));
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+    
+});
 
 app.listen(port, () => console.log(`PS Project Running on port ${port}!`));
