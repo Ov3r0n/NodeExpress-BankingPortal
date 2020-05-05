1728b0ddb224768632dab1e6004e5e943a814952
juanantonio.capel@hotmail.com <juanantonio.capel@hotmail.com>
2020-05-05 12:17:53 +0100

    M5

--- src/routes/services.js
+++ src/routes/services.js
@@ -3,18 +3,18 @@ const { accounts, writeJSON } = require('../data');
 
 const router = express.Router();
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('/transfer', (req, res) => {
+router.get('/transfer', (req, res) => res.render('transfer'));
+router.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     writeJSON(accounts);
     res.render('transfer', { message: 'Transfer Completed' });
 });
 
-app.get('/payment', (req, res) => {
+router.get('/payment', (req, res) => {
     res.render('payment', { account: accounts.credit })
 });
-app.post('/payment', (req, res) => {
+router.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
     writeJSON(accounts);
