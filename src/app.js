a8df7090fdb8c59ac37487595c0be87d5e7e18a0
juanantonio.capel@hotmail.com <juanantonio.capel@hotmail.com>
2020-05-05 12:10:44 +0100

    M4

--- src/app.js
+++ src/app.js
@@ -16,9 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.get('/transfer', (req, res) => res.render('transfer'));
--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'UTF8' });
 const users = JSON.parse(userData);
 
-const writeJSON = () => {
+const writeJSON = (accounts) => {
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 }
