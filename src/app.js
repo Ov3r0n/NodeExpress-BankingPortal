11b302db807e0ffc91d333bc4005c1f44cf16be3
Juan Antonio Capel Vidal <juanantonio.capel@hotmail.com>
2020-05-05 10:57:56 +0100

    blah

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const app = express.app;
+const app = express();
+const port = 3000;
 
+// Static files
+app.use(express.static(path.join(__dirname, '/src/public')));
+//app.use('/css', express.static(path.join(__dirname, '/src/public/css')));
+//app.use('/js', express.static(path.join(__dirname, '/src/public/js')));
+
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Index'});
+})
+
+app.listen(port, () => console.log(`PS Project Running on port ${port}!`));
